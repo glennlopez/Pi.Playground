face8e19ac83ca51eb5497a295eda676c0bc49c7
glennlopez <glennlopez@gmail.com>
2017-01-18 23:28:22 -0800

    update

--- prototypes/steperMotor_fsm.c
+++ prototypes/steperMotor_fsm.c
@@ -9,7 +9,7 @@
 
 struct fsmData{
 	void (*out)(void);
-	unsigned int delay;
+	long delay;
 	unsigned int next[4];
 
 }; typedef const struct fsmData FSM;
@@ -21,7 +21,19 @@ void S2(); void S5(); void S8();
 void S3(); void S6(); void S9();
 
 FSM Motor[10] = {
-	{&S0,	1,	{1,1,1,1}},
+	{&S0,	0.05,	{1,1,1,1}},
+
+	{&S1,	1,	{1,1,1,1}},
+	{&S2,	1,	{1,1,1,1}},
+	{&S3,	1,	{1,1,1,1}},
+
+	{&S4,	1,	{1,1,1,1}},
+	{&S5,	1,	{1,1,1,1}},
+	{&S6,	1,	{1,1,1,1}},
+
+	{&S7,	1,	{1,1,1,1}},
+	{&S8,	1,	{1,1,1,1}},
+	{&S9,	1,	{1,1,1,1}},
 };
 
 unsigned int cState, input, output;
@@ -40,18 +52,18 @@ int main (){
       return 1;
    }
 
-
 	//GPIO setup: 5,6,13,26
 	gpioSetMode(5, PI_OUTPUT);
 	gpioSetMode(6, PI_OUTPUT);
 	gpioSetMode(13, PI_OUTPUT);
 	gpioSetMode(26, PI_OUTPUT);
 
-	gpioSetMode(27, PI_OUTPUT);	//debug
-
 
 	while(1){
-		Motor[0].out();
+		gpioWrite(27, 1);
+		time_sleep(Motor[0].delay);
+		gpioWrite(27, 0);
+		time_sleep(Motor[0].delay);
 
 	}
 
@@ -70,5 +82,5 @@ int main (){
 	FUNCTION
 ***************/
 void S0(){
-   gpioWrite(27, 1); /* on */
+   gpioWrite(5, 1);
 }
